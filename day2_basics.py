4fa9c6ea34da5bd1c7105f944fa3bbcee35166cb
Vismaya Achyutha <visuachyu15@gmail.com>
2025-12-24 20:28:43 +0530

    day_2 practice

---
+++ day2_basics.py
@@ -0,0 +1,17 @@
+#day2
+#1.variables
+name="Git hub learners"
+age=18
+print(name,age)
+
+#2.simple calculation
+a=5
+b=5
+print("sum:",a+b)
+
+3.#if condition
+if age>=18:
+  print("eligible")
+else:
+  print("Not eligible")
+
